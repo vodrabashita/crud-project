142ad32bfd15a62859534198749bbd34e2b68ef6
vodrabashita <vodrabashita07@gmail.com>
2026-04-25 20:42:02 +0530

    Initialize Express server with authentication routes

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./auth");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
